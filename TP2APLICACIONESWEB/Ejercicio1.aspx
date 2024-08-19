2e3547e25538588b05b5f6b084ad9063961f1cd5
maxiifabeiro <machilux_08@hotmail.com>
2024-08-19 15:44:46 -0300

    Agregar archivos de proyecto.

---
+++ TP2APLICACIONESWEB/Ejercicio1.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio1.aspx.cs" Inherits="TP2APLICACIONESWEB.Ejercicio1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            &nbsp;&nbsp;Ingrese nombre del producto:&nbsp;
+            <asp:TextBox ID="txtProducto1" runat="server"></asp:TextBox>
+&nbsp;&nbsp; Cantidad:&nbsp;&nbsp;
+            <asp:TextBox ID="txtCantidad1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+&nbsp; Ingrese nombre del producto:&nbsp;
+            <asp:TextBox ID="txtProducto2" runat="server"></asp:TextBox>
+&nbsp;&nbsp; Cantidad:&nbsp;&nbsp;
+            <asp:TextBox ID="txtCantidad2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+&nbsp;&nbsp;
+            <asp:Button ID="btnTabla" runat="server" OnClick="btnTabla_Click" Text="Generar tabla" />
+            <br />
+            <br />
+&nbsp;
+            <asp:Label ID="lblTabla" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ TP2APLICACIONESWEB/Ejercicio1.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TP2APLICACIONESWEB
+{
+    public partial class Ejercicio1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnTabla_Click(object sender, EventArgs e)
+        {
+            string prodUno = txtProducto1.Text;
+            string prodDos = txtProducto2.Text;
+            int numeroUno = int.Parse(txtCantidad1.Text);
+            int numeroDos = int.Parse(txtCantidad2.Text);
+
+            String tabla = "<table border='1'>";
+            tabla += "<tr><td;>Producto</td> <td>Cantidad</td></tr>";
+
+            // Fila para el primer producto
+            tabla += "<tr>";
+            tabla += "<td>" + prodUno.ToString() + "</td>";
+            tabla += "<td>" + numeroUno + "</td>";
+            tabla += "</tr>";
+
+            // Fila para el segundo producto
+            tabla += "<tr>";
+            tabla += "<td>" + prodDos.ToString() + "</td>";
+            tabla += "<td>" + numeroDos + "</td>";
+            tabla += "</tr>";
+
+            // Fila para el total
+            tabla += "<tr>";
+            tabla += "<td>TOTAL</td>";
+            tabla += "<td>" + (numeroUno + numeroDos) + "</td>";
+            tabla += "</tr>";
+
+            tabla += "</table>";
+            lblTabla.Text = tabla;
+        }
+
+    }
+}
